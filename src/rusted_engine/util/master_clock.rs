fd5029caf85cf002a386e87b0325e159513b9052
Varrxion <Jordan3827@gmail.com>
2025-02-14 23:20:28 -0500

    Add cap to max delta time to ensure consistent physics

--- src/rusted_engine/util/master_clock.rs
+++ src/rusted_engine/util/master_clock.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 pub struct MasterClock {
     last_time: Instant,
     delta_time: f32,
+    max_delta: f32, // Maximum allowed delta time, for preventing clips at low framerates
 }
 
 impl MasterClock {
@@ -11,13 +12,16 @@ impl MasterClock {
         Self {
             last_time: Instant::now(),
             delta_time: 0.0,
+            max_delta: 0.1,
         }
     }
 
     /// Updates the clock by calculating the delta time since the last update.
     pub fn update(&mut self) {
         let current_time = Instant::now();
-        self.delta_time = current_time.duration_since(self.last_time).as_secs_f32();
+        let elapsed = current_time.duration_since(self.last_time).as_secs_f32();
+
+        self.delta_time = elapsed.min(self.max_delta);
         self.last_time = current_time;
     }
 
